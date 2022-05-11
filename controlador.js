c13bf88359b76ab41d33019316629bfb5dd6793a
Your Name <you@example.com>
2022-05-11 17:13:58 -0500

    ok

--- controlador.js
+++ controlador.js
@@ -1,107 +1,137 @@
 //OBJETOS
 let producto1={
-    nombre:"producto1",
+    nombre:"Fabuloso",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/1.jfif?alt=media&token=248c2c38-0560-4169-b2c9-6bde58a82037"
 }
 
 let producto2={
-    nombre:"producto2",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Dannon creamy",
+    precio:3400,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/2.jfif?alt=media&token=dbc18dc6-3dce-422b-8eb0-f31b0443afe3"
 }
 
 let producto3={
-    nombre:"producto3",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Nutri leche",
+    precio:1200,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/3.jfif?alt=media&token=61bfd608-44bc-4f46-b5fd-1ca2cc5febc3"
 }
 
 let producto4={
-    nombre:"producto4",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Caldo de pollo",
+    precio:5000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/4.jfif?alt=media&token=045ae4aa-4338-4bb8-91f3-18b941a445b9"
 }
 
 let producto5={
-    nombre:"producto5",
+    nombre:"Papel higienico sussex",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/5.jpg?alt=media&token=507868e8-68ba-46ce-bde7-ae68f882a027"
 }
 
 let producto6={
-    nombre:"producto6",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"ponquei clasico",
+    precio:1000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/6.jpg?alt=media&token=a2863afd-ce36-4ec3-ae2b-fbe130d06648"
 }
 
 let producto7={
-    nombre:"producto7",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Chips Ahoy",
+    precio:1700,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/7.jpg?alt=media&token=007837fb-565e-4c81-9d17-1343fb3b7be8"
 }
 
 let producto8={
-    nombre:"producto8",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Oreo",
+    precio:2500,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/8.jfif?alt=media&token=aa9c90ac-f814-4467-8623-696b1fdbde21"
 }
 
 let producto9={
-    nombre:"producto9",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Deliciosas Galletas",
+    precio:1400,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/9.png?alt=media&token=5b35e0a1-1a51-4bb4-a135-c3b97ee25151"
 }
 
 let producto10={
-    nombre:"producto10",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Salsa de maiz dulce",
+    precio:1800,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/10.jfif?alt=media&token=423757e1-5fe1-48e2-ac38-18c2609d7804"
 }
 
 let producto11={
-    nombre:"producto11",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Cocacola",
+    precio:1500,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/11.png?alt=media&token=4020b781-be1c-437a-8f70-b30a1a298505"
 }
 
 let producto12={
-    nombre:"producto12",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"Lomitos de tilapia",
+    precio:6000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/12.jfif?alt=media&token=8a21e3e0-02d0-4971-956d-97ee3a7ca738"
 }
 
 let producto13={
-    nombre:"producto13",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"PALMITOS DE MAR/CANGREJO ",
+    precio:8200,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/13.jfif?alt=media&token=62f44043-b6e9-4add-bb58-179905d01f83"
 }
 
 let producto14={
-    nombre:"producto14",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"trident",
+    precio:2000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/14.jfif?alt=media&token=9e4c87ad-1310-4b57-b2fb-8008c7b6a2bf"
 }
 
 let producto15={
-    nombre:"producto15",
+    nombre:"Mont blanc",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/15.jfif?alt=media&token=69efb404-39d4-4578-9be8-7195a55f8d4b"
+}
+let producto16={
+    nombre:"Cigarillos",
+    precio:6000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/16.jfif?alt=media&token=9b5f6d00-bfc5-4ac8-b411-79fb953b6f9e"
+}
+let producto17={
+    nombre:"Barquillo Biscolata",
+    precio:1400,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/17.jfif?alt=media&token=f2cae1fb-4cb4-425b-b4eb-71feeb357854"
+}
+let producto18={
+    nombre:"Supercoco",
+    precio:3200,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/18.jfif?alt=media&token=97cb06f9-4c05-43d0-b9b1-5757cb1f7f5f"
+}
+let producto19={
+    nombre:"espaguetis la muñeca",
+    precio:4300,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/19.jfif?alt=media&token=eea72120-6c92-46ab-afe4-bac8a4416746"
+}
+let producto20={
+    nombre:"Jumbo",
+    precio:2500,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/20.jfif?alt=media&token=e0f5a32f-f16b-473b-aacd-1f883ab89b7b"
 }
 
 //ARREGLO
@@ -120,11 +150,30 @@ let productos=Array(
     producto12,
     producto13,
     producto14,
-    producto15
+    producto15,
+    producto16,
+    producto17,
+    producto18,
+    producto19,
+    producto20
 )
+let contenedor=document.getElementById("contenedor")
+
 
 //RECORRO EL ARREGLO
+
 productos.forEach(function(producto){
-    console.log(producto.nombre)
+    let foto=document.createElement("img")
+    foto.classList.add("w-100","img-fluid")
+    foto.src=producto.foto
+    
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card")
+    tarjeta.appendChild(foto)
+    columna.appendChild(tarjeta)
+    contenedor.appendChild(columna)
 })
 
