f0582f3d12a810b1c5e05556a56608a852cde94e
Your Name <you@example.com>
2022-05-17 17:20:24 -0500

    super

--- controlador.js
+++ controlador.js
@@ -3,6 +3,7 @@ let producto1={
     nombre:"Fabuloso",
     precio:500000,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/1.jfif?alt=media&token=248c2c38-0560-4169-b2c9-6bde58a82037"
 }
 
@@ -10,6 +11,7 @@ let producto2={
     nombre:"Dannon creamy",
     precio:3400,
     estado:true,
+    pais:"inglaterra",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/2.jfif?alt=media&token=dbc18dc6-3dce-422b-8eb0-f31b0443afe3"
 }
 
@@ -17,6 +19,7 @@ let producto3={
     nombre:"Nutri leche",
     precio:1200,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/3.jfif?alt=media&token=61bfd608-44bc-4f46-b5fd-1ca2cc5febc3"
 }
 
@@ -24,6 +27,7 @@ let producto4={
     nombre:"Caldo de pollo",
     precio:5000,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/4.jfif?alt=media&token=045ae4aa-4338-4bb8-91f3-18b941a445b9"
 }
 
@@ -31,6 +35,7 @@ let producto5={
     nombre:"Papel higienico sussex",
     precio:500000,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/5.jpg?alt=media&token=507868e8-68ba-46ce-bde7-ae68f882a027"
 }
 
@@ -38,6 +43,7 @@ let producto6={
     nombre:"ponquei clasico",
     precio:1000,
     estado:true,
+    pais:"estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/6.jpg?alt=media&token=a2863afd-ce36-4ec3-ae2b-fbe130d06648"
 }
 
@@ -45,6 +51,7 @@ let producto7={
     nombre:"Chips Ahoy",
     precio:1700,
     estado:true,
+    pais:"mexico",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/7.jpg?alt=media&token=007837fb-565e-4c81-9d17-1343fb3b7be8"
 }
 
@@ -52,6 +59,7 @@ let producto8={
     nombre:"Oreo",
     precio:2500,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/8.jfif?alt=media&token=aa9c90ac-f814-4467-8623-696b1fdbde21"
 }
 
@@ -59,6 +67,7 @@ let producto9={
     nombre:"Deliciosas Galletas",
     precio:1400,
     estado:true,
+    pais:"chile",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/9.png?alt=media&token=5b35e0a1-1a51-4bb4-a135-c3b97ee25151"
 }
 
@@ -66,6 +75,7 @@ let producto10={
     nombre:"Salsa de maiz dulce",
     precio:1800,
     estado:true,
+    pais:"noruega",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/10.jfif?alt=media&token=423757e1-5fe1-48e2-ac38-18c2609d7804"
 }
 
@@ -73,6 +83,7 @@ let producto11={
     nombre:"Cocacola",
     precio:1500,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/11.png?alt=media&token=4020b781-be1c-437a-8f70-b30a1a298505"
 }
 
@@ -80,6 +91,7 @@ let producto12={
     nombre:"Lomitos de tilapia",
     precio:6000,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/12.jfif?alt=media&token=8a21e3e0-02d0-4971-956d-97ee3a7ca738"
 }
 
@@ -87,6 +99,7 @@ let producto13={
     nombre:"PALMITOS DE MAR/CANGREJO ",
     precio:8200,
     estado:true,
+    pais:"brasil",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/13.jfif?alt=media&token=62f44043-b6e9-4add-bb58-179905d01f83"
 }
 
@@ -94,6 +107,7 @@ let producto14={
     nombre:"trident",
     precio:2000,
     estado:true,
+    pais:"estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/14.jfif?alt=media&token=9e4c87ad-1310-4b57-b2fb-8008c7b6a2bf"
 }
 
@@ -101,38 +115,264 @@ let producto15={
     nombre:"Mont blanc",
     precio:500000,
     estado:true,
+    pais:"australia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/15.jfif?alt=media&token=69efb404-39d4-4578-9be8-7195a55f8d4b"
 }
 let producto16={
     nombre:"Cigarillos",
     precio:6000,
     estado:true,
+    pais:"canada",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/16.jfif?alt=media&token=9b5f6d00-bfc5-4ac8-b411-79fb953b6f9e"
 }
 let producto17={
     nombre:"Barquillo Biscolata",
     precio:1400,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/17.jfif?alt=media&token=f2cae1fb-4cb4-425b-b4eb-71feeb357854"
 }
 let producto18={
     nombre:"Supercoco",
     precio:3200,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/18.jfif?alt=media&token=97cb06f9-4c05-43d0-b9b1-5757cb1f7f5f"
 }
 let producto19={
     nombre:"espaguetis la muñeca",
     precio:4300,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/19.jfif?alt=media&token=eea72120-6c92-46ab-afe4-bac8a4416746"
 }
 let producto20={
     nombre:"Jumbo",
     precio:2500,
     estado:true,
+    pais:"colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/20.jfif?alt=media&token=e0f5a32f-f16b-473b-aacd-1f883ab89b7b"
+}    
+let producto21={
+    nombre:"Jumbo",
+    precio:2500,
+    estado:true,
+    pais:"colombia",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/AGUA%20CON%20GAS%20LIMONADA%20OMI.jfif?alt=media&token=7b6e21a1-6c70-4069-a788-5aa4dd1ecd43"
 }
+let producto22={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:"https://firebasestorage.googleapis.com/v0/b/tienda-695ba.appspot.com/o/AGUARDIENTE%20AMARILLO%20MANZANARES.jfif?alt=media&token=a1e94f34-6d7c-4b32-afbf-0c0aa30202db"
+}
+let producto23={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto24={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto25={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto26={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto27={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto28={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto29={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto30={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto31={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto32={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto33={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto34={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto35={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto36={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto37={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto38={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto39={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto40={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto41={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto42={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto43={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto44={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto45={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto46={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto47={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto48={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto49={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto50={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+let producto20={
+nombre:"Jumbo",
+precio:2500,
+estado:true,
+pais:"colombia",
+foto:""
+}
+
+    
+
 
 //ARREGLO
 let productos=Array(
@@ -155,7 +395,38 @@ let productos=Array(
     producto17,
     producto18,
     producto19,
-    producto20
+    producto20,
+    producto21,
+    producto22,
+    producto23,
+    producto24,
+    producto25,
+    producto26,
+    producto27,
+    producto28,
+    producto29,
+    producto30,
+    producto31,
+    producto32,
+    producto33,
+    producto34,
+    producto35,
+    producto36,
+    producto37,
+    producto38,
+    producto39,
+    producto40,
+    producto41,
+    producto42,
+    producto43,
+    producto44,
+    producto45,
+    producto46,
+    producto47,
+    producto48,
+    producto49,
+    producto50
+    
 )
 let contenedor=document.getElementById("contenedor")
 
@@ -166,14 +437,32 @@ productos.forEach(function(producto){
     let foto=document.createElement("img")
     foto.classList.add("w-100","img-fluid")
     foto.src=producto.foto
+
+    let nombre=document.createElement("h5")
+    estado.classList.add("text-center")
+    nombre.textContent=producto.nombre
+
+    let estado=document.createElement("p")
+    estado.classList.add("text-center")
+    nombre.textContent=producto.estado
+
+    let precio=document.createElement("p")
+    estado.classList.add("text-center")
+    nombre.textContent="$"+producto.precio
+
     
     let columna=document.createElement("div")
     columna.classList.add("col")
 
     let tarjeta=document.createElement("div")
     tarjeta.classList.add("card")
+
     tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombre)
+    tarjeta.appendChild(precio)
+    tarjeta.appendChild(estado)
     columna.appendChild(tarjeta)
     contenedor.appendChild(columna)
+
 })
 
